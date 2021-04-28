ed98b7bf117a95716bbc0c59e6b75888b95aa175
Dean Hildebrand <dean.hildebrand88@gmail.com>
2021-04-28 14:44:38 -0400

    facotry and constructor functions created and practiced

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const address = {
   zipCode: "29577",
 };
 
+// iterate with a for in loop and log key and value
 function showAddress(address) {
   for (let keys in address) {
     console.log(keys, ":", address[keys]);
@@ -11,3 +12,36 @@ function showAddress(address) {
 }
 
 showAddress(address);
+
+//Factory function ----------------------------------------------
+
+function createAddress(street, city, zipCode) {
+  return {
+    street,
+    city,
+    zipCode,
+  };
+}
+
+let factoryAddress = createAddress(
+  "1211 Hadley Circle",
+  "Myrtle Beach",
+  "29577"
+);
+
+console.log(factoryAddress);
+
+//Constructor function ---------------------------------------------
+function Address(street, city, zipCode) {
+  this.street = street;
+  this.city = city;
+  this.zipCode = zipCode;
+}
+
+let constructorAddress = new Address(
+  "1211 Hadley Circle",
+  "Myrtle Beach",
+  "29577"
+);
+
+console.log(constructorAddress);
